153d6bd007d2734e3008781098797ee35c4aced4
Rafael Mello <almeidamellorafael@gmail.com>
2026-04-27 22:09:35 -0300

    add

---
+++ main.py
@@ -0,0 +1,9 @@
+# 1° Exercício
+numero = float(input("Digite um numero: "))
+
+if numero > 0:
+    print(f"{numero} é positivo!")
+elif numero < 0:
+    print(f"{numero} é negativo!")
+else:
+    print(f"{numero} é zero!")
