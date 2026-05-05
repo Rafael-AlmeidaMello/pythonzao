aadfd6c04d932873ede1343d7336c509e855c2fa
Rafael-AlmeidaMello <almeidamellorafael@gmail.com>
2026-05-04 22:24:12 -0300

    add exercises

--- main.py
+++ main.py
@@ -1,9 +1,78 @@
 # 1° Exercício
-numero = float(input("Digite um numero: "))
+#numero = float(input("Digite um numero: "))
 
-if numero > 0:
-    print(f"{numero} é positivo!")
-elif numero < 0:
-    print(f"{numero} é negativo!")
-else:
-    print(f"{numero} é zero!")
+#if numero > 0:
+#    print(f"{numero} é positivo!")
+#elif numero < 0:
+#    print(f"{numero} é negativo!")
+#else:
+#    print(f"{numero} é zero!")
+
+# 2° Exercício
+#numero = float(input("digite um numero: "))
+
+#if numero % 2 == 0:
+#    print("o numero é par")
+#else:
+#    print("o numero é impar")
+
+# 3° Exercício
+
+#numero1 = float(input("digite o primeiro numero: "))
+#numero2 = float(input("digite o segundo numero: "))
+
+#if numero1 > numero2:
+#    print(f"O maior numero é {numero1}")
+#else:
+#    print(f"O maior numero é {numero2}")
+ 
+# 4° Exercício
+       
+#numero1 = float(input("digite o primeiro numero: "))
+#numero2 = float(input("digite o segundo numero: "))
+#numero3 = float(input("digite o terceiro numero: "))
+
+#if numero1 > numero2 and numero3:
+#    print(f"O maior numero é {numero1}")
+#elif numero2 > numero1 and numero3:
+#    print(f"O maior numero é {numero2}")
+#else:
+#        print(f"O maior numero é {numero3}")
+
+# 5° Exercício
+
+#nota1 = float(input("digite a primeira nota: "))
+#nota2 = float(input("digite a segunda nota: "))
+#media = (nota1 + nota2) / 2
+
+#if media >= 7:
+#    print("você foi aprovado!")
+#elif media < 7 and media >= 5:
+#    print("você está em recuperação")
+#else:
+#    print("você foi reprovado")
+
+# 6° Exercício
+
+#idade = int(input("digite sua idade: "))
+
+#if idade < 18:
+#    print("voce é uma criança")
+#elif idade >= 18 and idade <= 59:
+#    print("voce é um adulto")
+#else: print("voce é um idoso")
+
+# 7° Exercício
+
+#salario = float(input("Digite seu salário: "))
+#desconto10 = salario * 0.90
+#desconto5 = salario * 0.95
+
+#if salario > 5000:
+#    print(f"seu salario liquido é {desconto10:.2f}")
+#else:
+#    print(f"seu salario liquido é {desconto5:.2f}")
+
+# 8 Exercício
+    
+
